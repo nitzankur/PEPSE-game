97c6fae8a8b8e4da284707a79a2d507d7751aab9
Micha Hashkes <mhashkes@gmail.com>
2022-12-19 11:10:23 +0200

    added sky and initializeGame

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,8 +1,26 @@
 package pepse;
 
 import danogl.GameManager;
+import danogl.GameObject;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import danogl.util.Vector2;
+import pepse.world.Sky;
 
 public class PepseGameManager extends GameManager {
+//    public PepseGameManager(String windowTitle, Vector2 windowDimensions) {
+//        super(windowTitle, windowDimensions);
+//    }
+
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader,
+                               UserInputListener inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+
+        Sky.create(gameObjects(), windowController.getWindowDimensions(), 0);
+    }
 
     public static void main(String[] args){
         new PepseGameManager().run();
